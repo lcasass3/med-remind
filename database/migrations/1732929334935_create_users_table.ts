94dcbb3e13da130cac8b79279a706da8ef1f8789
Leon Casas <leonn.casass@gmail.com>
2024-11-30 06:40:54 -0600

    fix users table

--- database/migrations/1732929334935_create_users_table.ts
+++ database/migrations/1732929334935_create_users_table.ts
@@ -5,11 +5,11 @@ export default class extends BaseSchema {
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id').notNullable()
+      table.increments('id').primary().unsigned().notNullable()
       table.string('full_name').nullable()
       table.string('email', 254).notNullable().unique()
       table.string('password').notNullable()
-
+      table.string('phone').nullable()
       table.timestamp('created_at').notNullable()
       table.timestamp('updated_at').nullable()
     })
